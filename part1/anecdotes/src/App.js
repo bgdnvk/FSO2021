aa8a2971a75e70c1855aab855299643dfa218ce7
bgdnvk <bnovykov@gmail.com>
2021-04-03 03:14:35 +0200

    1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -11,6 +11,15 @@ const Button = ({text, handleClick}) => {
   )
 }
 
+const Anecdote = ({title, text}) => {
+  return(
+    <div>
+      <h1>{title}</h1>
+      <p>{text}</p>
+    </div>
+  )
+}
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often',
@@ -20,19 +29,40 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
+  //array of points for anecdotes
+  const points = [...Array(6).fill(0)]
    
   const [selected, setSelected] = useState(0)
+  const [getPoints, setPoints] = useState(points)
+  console.log("points", getPoints);
 
   const randomNum = () => Math.floor(Math.random() * anecdotes.length)
   const anecdoteClick = () => setSelected(randomNum())
-  
+  const pointsClick = () => {
+    //not altering the state
+    const copyPoints = [...getPoints] 
+    copyPoints[selected] = copyPoints[selected]+=1
+    setPoints(copyPoints)
+  }
+  const getMostVoted = () => getPoints.indexOf(Math.max(...getPoints))
 
   return (
     <div>
-      {anecdotes[selected]}
+      <Anecdote
+      title={"Anecdote of the day"}
+      text={anecdotes[selected]}
+      ></Anecdote>
       <Button text={"next anecdote"}
       handleClick={anecdoteClick}
       ></Button>
+      <Button text={"vote"}
+      handleClick={pointsClick}
+      ></Button>
+
+      <Anecdote
+      title={"Anecdote with the most votes"}
+      text={anecdotes[getMostVoted()]}
+      ></Anecdote>
     </div>
   )
 }
