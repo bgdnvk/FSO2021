6589abeb5c4dc3d39b044788040168313f85310b
bgdnvk <bnovykov@gmail.com>
2021-04-04 18:47:15 +0200

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,10 +1,24 @@
 import React, { useState } from 'react'
 
+const DisplayData = ({arr}) => {
+  return(
+    <div>
+      {arr.map( entry => <div> {entry.name} - {entry.number}</div>)}
+    </div>
+  )
+  
+
+}
+
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const addNote = (e) => {
     e.preventDefault()
@@ -16,20 +30,24 @@ const App = () => {
     }
 
     const newNameObj = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     //better than concat?
     console.log("obj is", newNameObj);
     setPersons([...persons, newNameObj])
     setNewName('')
+    setNewNumber('')
     console.log(persons);
   }
-  const handleNameChange = (e) => {
+  const handleChange = (e, setter) => {
     e.preventDefault()
-    console.log("namechange", e.target.value);
-    setNewName(e.target.value)
+    console.log("handlechange", e.target.value);
+    setter(e.target.value)
   }
   const checkDupe = (arr, newName) => arr.find( fullname => fullname.name === newName)
+
+ 
 
   return (
     <div>
@@ -37,8 +55,15 @@ const App = () => {
       <form onSubmit={addNote}>
         <div>
           name: <input
-          onChange={handleNameChange}
+          onChange={ (e) => handleChange(e, setNewName)}
           value={newName} />
+        </div>
+        <div>
+          number: <input
+          onChange = { (e) => {handleChange(e, setNewNumber)}}
+          value = {newNumber}
+          >
+          </input>
         </div>
         <div>
           <button type="submit">add</button>
@@ -46,9 +71,8 @@ const App = () => {
       </form>
 
       <h2>Numbers</h2>
-      {persons.map(
-        person => <div> {person.name} </div>
-      )}
+      <DisplayData arr={persons}></DisplayData>
+
     </div>
   )
 }
