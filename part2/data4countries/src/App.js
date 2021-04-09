f8d139136c88cbc54a711b2db1667c9a8d1ff5ac
bgdnvk <bnovykov@gmail.com>
2021-04-09 04:41:21 +0200

    countries

---
+++ part2/data4countries/src/App.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+function App() {
+  const countriesUrl = 'https://restcountries.eu/rest/v2/all'
+  
+  const [countries, setCountries] = useState('')
+
+  const effectHook = () => axios
+        .get(countriesUrl).then((res) => setCountries(res.data))
+  useEffect(effectHook, [])
+
+  console.log(countries);
+  console.log(countries[0]);
+  return (
+    <div>
+      {
+        countries ? countries[0].name: "loading"
+      }
+    </div>
+  );
+}
+
+export default App;
